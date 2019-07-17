648971d3316013bd848f3d4be2189859ddc78065
Jenesh <jenesh.napit@gmail.com>
2019-07-16 21:20:33 -0400

    proj0 progress again

--- proj0/Body.java
+++ proj0/Body.java
@@ -5,6 +5,7 @@ public class Body {
     public double yyVel = 4.0;
     public double mass = 5.0;
     public String imgFileName = "jupiter.gif";
+    public static double gravitationalPull = 6.67e-11;
 
     public Body(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
@@ -16,15 +17,38 @@ public class Body {
     }
 
     public Body(Body b) {
-        Body(b);
+        
     }
 
     public double calcDistance(Body pos2) {
-        double xx = Math.pow((this.xxPos - pos2.xxPos), 2);
-        double yy = Math.pow((this.yyPos - pos2.yyPos), 2);
+        double xx = Math.pow(( pos2.xxPos - this.xxPos), 2);
+        double yy = Math.pow(( pos2.yyPos - this.yyPos), 2);
         double sum = xx + yy;
         sum = Math.sqrt(sum);
 
         return sum;
+    }
+
+    public double calcForceExertedBy(Body b2) {
+        double force = (gravitationalPull * this.mass * b2.mass) / Math.pow(calcDistance(b2),2);
+        return force;
+    }
+
+    public double calcForceExertedByX(Body b2) {
+        double pairWiseForce = calcForceExertedBy(b2);
+        double radius = calcDistance(b2);
+        // double forceX1 = pairWiseForce * this.xxVel / radius;
+        double forceX = pairWiseForce * b2.xxVel / radius;
+
+        return forceX;
+    }
+
+    public double calcForceExertedByY(Body b2) {
+        double pairWiseForce = calcForceExertedBy(b2);
+        double radius = calcDistance(b2);
+        // double forceY1 = pairWiseForce * this.yyVel / radius;
+        double forceY = pairWiseForce * b2.yyVel / radius;
+        // double netForceY = forceY1 + forceY2;
+        return forceY;
     }
 }
